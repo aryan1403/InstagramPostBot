67a8ebc1814060a0ce9b03061387764dcd709cb3
Aaryan <aaryan14032006@gmail.com>
2021-07-23 08:49:32 +0530

    Configuration file created

---
+++ src/main/java/com/example/Helpers/configuration.java
@@ -0,0 +1,14 @@
+package com.example.Helpers;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvBuilder;
+
+public class configuration {
+
+    private static final Dotenv dotenv = new DotenvBuilder().ignoreIfMissing().load();
+
+    public static String botToken = dotenv.get("botToken");
+    public static String botUserName = dotenv.get("botUserName");
+    public static String handler = dotenv.get("handler");
+    public static String rapidXapi = dotenv.get("rapidXapi");
+}
