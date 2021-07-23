0ea30b42f85f91c4f7292363947ab62be642b4db
Aaryan <aaryan14032006@gmail.com>
2021-07-23 15:27:45 +0530

    Updated configuration file

--- src/main/java/com/example/Helpers/configuration.java
+++ src/main/java/com/example/Helpers/configuration.java
@@ -10,5 +10,6 @@ public class configuration {
     public static String botToken = dotenv.get("botToken");
     public static String botUserName = dotenv.get("botUserName");
     public static String handler = dotenv.get("handler");
-    public static String rapidXapi = dotenv.get("rapidXapi");
+    public static String username = dotenv.get("username");
+    public static String password = dotenv.get("password");
 }
