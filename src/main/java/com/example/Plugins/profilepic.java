21d0407474200c4389d465b19a9009872be7536c
Aaryan <aaryan14032006@gmail.com>
2021-07-23 20:46:59 +0530

    Fixed some lol

--- src/main/java/com/example/Plugins/profilepic.java
+++ src/main/java/com/example/Plugins/profilepic.java
@@ -2,9 +2,10 @@ package com.example.Plugins;
 
 
 import java.util.List;
+
+import com.example.Bot;
 import com.example.Master;
 import com.example.Helpers.login;
-import com.example.test.testBot;
 import com.github.instagram4j.instagram4j.IGClient;
 import org.telegram.telegrambots.meta.api.methods.GetFile;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -14,7 +15,7 @@ import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class profilepic extends testBot implements Master {
+public class profilepic extends Bot implements Master {
 
     @Override
     public void handleRequests(Update update, String cmd) {
