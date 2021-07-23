4e98a6caf5b7184e5fdd6a62473617db1ea709ca
Aaryan <aaryan14032006@gmail.com>
2021-07-23 08:50:34 +0530

    Tester configured

---
+++ src/main/java/com/example/test/TestRunner.java
@@ -0,0 +1,19 @@
+package com.example.test;
+
+import com.example.test.testBot;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class TestRunner {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new testBot());
+            System.out.println("Bot Started Successfully");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
