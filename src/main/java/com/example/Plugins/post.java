cdd264202b2710d44827507e46290cf3c4bf3fb4
Aaryan <aaryan14032006@gmail.com>
2021-07-24 14:17:35 +0530

    :/

--- src/main/java/com/example/Plugins/post.java
+++ src/main/java/com/example/Plugins/post.java
@@ -16,51 +16,50 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class post extends Bot implements Master {
     @Override
     public void handleRequests(Update update, String cmd) {
-        if (update.getMessage().getChat().isChannelChat()) {
-            String username = configuration.username;
-            String password = configuration.password;
-            try {
-                IGClient client = IGClient.builder().username(username).password(password).login();
-                SendMessage message = new SendMessage(update.getMessage().getChatId().toString(), "Downloading...");
+        String username = configuration.username;
+        String password = configuration.password;
+        try {
+            IGClient client = IGClient.builder().username(username).password(password).login();
+            SendMessage message = new SendMessage(update.getMessage().getChatId().toString(), "Downloading...");
 
-                execute(message);
+            execute(message);
 
-                List<PhotoSize> arr = update.getChannelPost().getPhoto();
+            List<PhotoSize> arr = update.getChannelPost().getPhoto();
 
-                PhotoSize biggSize = null;
-                for (int i = 0; i < arr.size(); i++) {
-                    for (int j = 0; j < arr.size(); j++) {
-                        if (arr.get(i).getFileSize() > arr.get(j).getFileSize())
-                            biggSize = arr.get(i);
-                    }
+            PhotoSize biggSize = null;
+            for (int i = 0; i < arr.size(); i++) {
+                for (int j = 0; j < arr.size(); j++) {
+                    if (arr.get(i).getFileSize() > arr.get(j).getFileSize())
+                        biggSize = arr.get(i);
                 }
-                String caption = update.getChannelPost().getCaption();
-                PhotoSize photos = biggSize;
-                GetFile getFiled = new GetFile();
-                getFiled.setFileId(photos.getFileId());
+            }
+            String caption = update.getChannelPost().getCaption();
+            PhotoSize photos = biggSize;
+            GetFile getFiled = new GetFile();
+            getFiled.setFileId(photos.getFileId());
 
-                org.telegram.telegrambots.meta.api.objects.File file;
+            org.telegram.telegrambots.meta.api.objects.File file;
 
-                file = execute(getFiled);
-                File file2 = downloadFile(file);
+            file = execute(getFiled);
+            File file2 = downloadFile(file);
 
-                SendMessage message52 = new SendMessage(update.getMessage().getChatId().toString(), "Uploading...");
+            SendMessage message52 = new SendMessage(update.getMessage().getChatId().toString(), "Uploading...");
 
-                execute(message52);
+            execute(message52);
 
-                client.actions().timeline().uploadPhoto(file2, caption).thenAccept(response -> {
-                    SendMessage message526 = new SendMessage(update.getMessage().getChatId().toString(), "Uploaded Successfully");
+            client.actions().timeline().uploadPhoto(file2, caption).thenAccept(response -> {
+                SendMessage message526 = new SendMessage(update.getMessage().getChatId().toString(),
+                        "Uploaded Successfully");
 
                 try {
                     execute(message526);
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
-                }).join();
+            }).join();
 
-            } catch (IGLoginException | TelegramApiException e) {
-                e.printStackTrace();
-            }
+        } catch (IGLoginException | TelegramApiException e) {
+            e.printStackTrace();
         }
 
     }
